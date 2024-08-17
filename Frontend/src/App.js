fa3471564c3f5ed94da0d4d13205c24624c9ec65
Piyush Patel <piyushpatel9165@gmail.com>
2024-08-17 19:55:41 +0530

    loader

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -7,14 +7,14 @@ import Loader from "./components/loader/Loader";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Navbar } from "./components/Navbar components/Navbar";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useContext } from "react";
 import { UserContext } from "./context/context";
-
-
+import { useLocation } from "react-router-dom";
 
 function App() {
   const { user, loader, setLoader } = useContext(UserContext);
+  const location = useLocation();
 
   useEffect(() => {
     setLoader(true);
@@ -24,12 +24,23 @@ function App() {
     };
   }, []);
 
+  useEffect(() => {
+    setLoader(true);
+
+    // Fake delay to simulate loading process
+    const timeoutId = setTimeout(() => {
+      setLoader(false);
+    }, 500); // Adjust the delay if needed
+
+    return () => clearTimeout(timeoutId);
+  }, [location.pathname]);
+
   return (
     <div className="duration-300 bg-[#f9f9f9] text-blue-950 dark:bg-slate-950 dark:text-white">
       <Navbar />
       {loader ? (
         <Loader />
-      ) :
+      ) : (
         <>
           <ScrollToTop />
           <AppRoutes user={user} />
@@ -37,8 +48,7 @@ function App() {
           <Toaster />
           <ToastContainer />
         </>
-      }
-
+      )}
     </div>
   );
 }
--- Frontend/src/components/Cards/CardSlider.jsx
+++ Frontend/src/components/Cards/CardSlider.jsx
@@ -57,7 +57,7 @@ const CardSlider = () => {
 
   useEffect(() => {
     getitems();
-  }, [url]);
+  }, []);
 
   return (
     <>
