5b0bda2106dc7278efb9c7551e9b1ea5b943f880
Piyush Patel <piyushpatel9165@gmail.com>
2024-08-25 21:29:41 +0530

    firebase credentials

--- Backend/utils/firebase.js
+++ Backend/utils/firebase.js
@@ -1,10 +1,13 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // Replace with the path to your Firebase service account key
+// const serviceAccount = require('./serviceAccountKey.json'); // Replace with the path to your Firebase service account key
+
+const firebaseCredentials = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(firebaseCredentials),
   storageBucket: 'gs://renify-bd804.appspot.com' // Replace with your Firebase storage bucket URL
 });
+
 
 const bucket = admin.storage().bucket();
 module.exports = bucket;
