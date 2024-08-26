42a2c463badf0c8a5ac6b7c2e19a2fa0f54ac22c
Piyush Patel <piyushpatel9165@gmail.com>
2024-08-26 22:21:33 +0530

    pull

--- Backend/app.js
+++ Backend/app.js
@@ -16,17 +16,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(cors())
-// ___________________________________________________________________________
+
 // const FRONTEND_ORIGIN = 'http://localhost:3000';
-const FRONTEND_ORIGIN = 'https://renify.vercel.app';
+// const FRONTEND_ORIGIN = 'https://renify.vercel.app';
 
-app.use(cors({
-  origin: FRONTEND_ORIGIN,
-  credentials: true // This ensures cookies are included in the requests
-}));
+// app.use(cors({
+//   origin: FRONTEND_ORIGIN,
+//   credentials: true // This ensures cookies are included in the requests
+// }));
+
 // ___________________________________________________________________________
-
-app.listen(4000)
 
 
 // ________________________________________
@@ -56,6 +55,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
+app.listen(4000)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
